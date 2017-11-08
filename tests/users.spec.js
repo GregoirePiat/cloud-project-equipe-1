aa057949d65af643f6095d38d1ef5cd59d468138
Nicolas Diab <nicolas.diab@etu.univ-lyon1.fr>
2017-11-08 16:27:07 +0100

    first workin test

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,6 +1,34 @@
 import test from 'ava';
+import express from 'express';
+
+var request = require('supertest');
+var bodyParser = require('body-parser');
 
 
+function makeApp() {
+    var app = express();
+    app.use(bodyParser.json());
+
+    app.get('/user', function(req, res) {
+        res.status(200).json({ name: 'tobi' });
+    });
+
+    return app;
+}
+
+test('test1', async t => {
+    t.plan(1);
+
+    const res = await request(makeApp())
+        .get('/user')
+        .send({email: 'ava@rocks.com', password: '123123'});
+
+    t.is(res.status, 200);
+   // t.is(res.json, 'tobi');
+});
+
+
+/* BELOW SOME EXEMPLES -> TODO DELETE BEFORE FINAL VERSION
 // This runs before all tests
 test.before(t => {
     t.pass();
@@ -18,4 +46,4 @@ test('bar', async t => {
 
 test('can add numbers', t => {
     t.is(1 + 1, 2);
-});
+});*/
