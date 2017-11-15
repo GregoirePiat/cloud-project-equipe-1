4f79166fd4db51de2e712ae51243f09c56b6afa2
Nicolas Diab <nicolas.diab@etu.univ-lyon1.fr>
2017-11-15 11:01:32 +0100

    Added file with some users to ease the tests

---
+++ tests/users.initialData.js
@@ -0,0 +1,9 @@
+users_initialData = [
+    {"firstName":"charley","lastName":"kuin","position":{"lat":32.234711,"lon":-51.304815},"birthDay":"10/12/1949"},
+    {"firstName":"arne","lastName":"hauser","position":{"lat":61.79961,"lon":-12.5041},"birthDay":"11/30/1952"},
+    {"firstName":"نازنین","lastName":"سالاری","position":{"lat":-77.064432,"lon":164.840946},"birthDay":"04/25/1952"},
+    {"firstName":"pascal","lastName":"bach","position":{"lat":77.79446,"lon":46.811293},"birthDay":"09/26/1971"},
+    {"firstName":"mikkel","lastName":"sørensen","position":{"lat":30.718215,"lon":146.489285},"birthDay":"07/29/1954"}
+];
+
+module.exports = users_initialData;
