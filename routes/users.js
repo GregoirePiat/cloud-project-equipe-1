866f1f289e18673b3aa2c5da44184bcefe979e37
DOLLE <jeremy.dolle34@gmail.com>
2017-11-15 09:53:45 +0100

    users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * GET
+ */
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.json({message:"Get a list of users"});
 });
+
+/* GET one user by id. */
+router.get('/:id', function(req, res, next) {
+    var userId = req.params.id;
+    res.json({message:"This is user " + userId});
+});
+
+
+/**
+ * POST
+ */
+/* POST - Create new user */
+router.post('/', function(req, res, next) {
+    res.json({message:"Adding a new user"}, 201);
+});
+
+/**
+ * PUT
+ */
+/* PUT - Update all user */
+router.put('/', function(req, res, next) {
+    res.json({message:"Update a list of users"}, 201);
+});
+
+/* PUT - Update one user by id */
+router.put('/:id', function(req, res, next) {
+    var userId = req.params.id;
+    res.json({message:"Update user " + userId.text()}, 201);
+});
+
+
+/**
+ * DELETE
+ */
+/* DELETE - Update all user */
+router.delete('/', function(req, res, next) {
+    res.json({message:"Delete a list of users"});
+});
+
+/* DELETE - Delete one user by id */
+router.delete('/:id', function(req, res, next) {
+    var userId = req.params.id;
+    res.json({message:"Delete user " + userId.text()});
+});
+
+
+
 
 module.exports = router;
