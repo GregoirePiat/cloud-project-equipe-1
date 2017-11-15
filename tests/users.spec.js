d93f51f77212e9a950521e9624fe2340ea8bcc03
Nicolas Diab <nicolas.diab@etu.univ-lyon1.fr>
2017-11-15 11:02:06 +0100

    Added 3 tests : getAll / getById / add

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,55 +1,109 @@
 import test from 'ava';
 import express from 'express';
+let users = require('../routes/users');
+
+let initialData = require('./users.initialData');
+let initialDataId = [];
 
 var request = require('supertest');
 var bodyParser = require('body-parser');
-var UserController = require('../controller/user.controller');
 
 
+// Create a app's new instance to avoid problems
 function makeApp() {
     var app = express();
     app.use(bodyParser.json());
-
-    app.get('/user', function(req, res) {
-        res.json({ name: 'tobi' });
-        res.status(200);
-    });
-
-    /*
-    app.get('/user',UserController.getAllUser);
-    */
-
+    app.use('/user', users);
     return app;
 }
 
-test('test1', async t => {
-    t.plan(2);
+//TODO : mock all necessary methods
+
+// This runs before all tests - add 5 users to use for tests
+test.before(async t => {
+    t.plan(initialData.length);
+
+    for (var i=0; i < initialData.length; i++) {
+        const res = await request(makeApp())
+            .post('/')
+            .send(initialData[i]);
+
+        t.is(res.text, i);
+        initialDataId[i] = res.text;
+    }
+
+    t.pass();
+});
+
+test('getAllUsers', async t => {
+    t.plan(1);
 
     const res = await request(makeApp())
-        .get('/user')
-        .send({email: 'ava@rocks.com', password: '123123'});
+        .get('/');
 
     t.is(res.status, 200);
-    t.is(res.text, JSON.stringify({ name: 'tobi' }));
+    t.is(res.text, JSON.stringify(initialData));
 });
 
 
-/* BELOW SOME EXEMPLES -> TODO DELETE BEFORE FINAL VERSION
-// This runs before all tests
-test.before(t => {
-    t.pass();
+test('getUserByID', async t => {
+    t.plan(2);
+
+    var midTabIndex = Math.floor(initialData.length / 2);
+    console.log(initialData[midTabIndex]);
+    console.log(initialDataId[midTabIndex]);
+
+    const resById = await request(makeApp())
+        .get('/' + initialDataId);
+
+    t.is(resById.status, 200);
+
+    //get all users to get id of added user
+    const resAll = await request(makeApp())
+        .get('/');
+
+    var addedUser = null;
+    for (var i = 0; i < resAll.text.length; i++) {
+        if (resAll.text[i].firstName == initialData[midTabIndex].firstName &&
+            resAll.text[i].lastName == initialData[midTabIndex].lastName &&
+            resAll.text[i].position == initialData[midTabIndex].position) {
+            addedUser = resAll.text[i];
+        }
+    }
+
+    // check getById result
+    t.is(resById.text, JSON.stringify(addedUser));
 });
 
-test('foo', t => {
-    t.pass();
+test('createUser', async t => {
+    t.plan(3);
+
+    // add first user from initial data
+    const resAdd = await request(makeApp())
+        .post('/')
+        .send(JSON.stringify(initialData[0]));
+
+    t.is(resAdd.status, 201);
+
+    // user was added correctly ?
+
+    //get all users to get id of added user
+    const resAll = await request(makeApp())
+        .get('/');
+
+    var addedUser = null;
+    for (var i = 0; i < resAll.text.length; i++) {
+        if (resAll.text[i].firstName == initialData[0].firstName &&
+            resAll.text[i].lastName == initialData[0].lastName &&
+            resAll.text[i].position == initialData[0].position) {
+            addedUser = resAll.text[i];
+        }
+    }
+
+    //get added user by id
+    const resById = await request(makeApp())
+        .get('/' + addedUser.id);
+
+    t.is(resById.text, JSON.stringify(addedUser));
 });
 
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-
-    t.is(await bar, 'bar');
-});
-
-test('can add numbers', t => {
-    t.is(1 + 1, 2);
-});*/
