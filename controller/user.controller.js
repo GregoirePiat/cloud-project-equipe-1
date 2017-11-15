a51a352aa40299f63cff2aab2fae8f10c605937e
agory <alexandre.gory@etu.univ-lyon1.fr>
2017-11-15 13:51:15 +0100

    Improve getUserById : Error management and correct id => _id

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,30 +3,29 @@ let User = require('../model/user');
 
 const dto2dao = user => {
 
-  return new User({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    birthDay: moment(user.birthDay, 'MM/DD/YYYY'),
-    position: {
-      type: "Point",
-      coordinates: [user.position.lon, user.position.lat]
-    }
-  });
+    return new User({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        birthDay: moment(user.birthDay, 'MM/DD/YYYY'),
+        position: {
+            type: 'Point',
+            coordinates: [user.position.lon, user.position.lat]
+        }
+    });
 };
 
 const dao2dto = User => {
-  return ({
-    firstName: User.firstName,
-    lastName: User.lastName,
-    id: User._id,
-    birthDay: moment(User.birthDay).format('L'),
-    position: {
-      lat: User.position.coordinates[1],
-      lon: User.position.coordinates[0],
-    }
-  });
+    return ({
+        firstName: User.firstName,
+        lastName: User.lastName,
+        id: User._id,
+        birthDay: moment(User.birthDay).format('L'),
+        position: {
+            lat: User.position.coordinates[1],
+            lon: User.position.coordinates[0],
+        }
+    });
 }
-
 
 
 let UserController = {
@@ -40,58 +39,69 @@ let UserController = {
 
     /* GET one user by id. */
     getUserByID: function(req, res, next) {
-        var userId = req.params.id;
-        var User = User.findOne({'id': userId}, function(err, user) {
-            res.json(dao2dto(user));
+        let userId = req.params.id;
+        console.log(userId);
+        User.findOne({'_id': userId}, function(err, user) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+                res.json({error: 'something went wrong with the database'})
+            }
+            if (user) {
+                res.json(dao2dto(user));
+            } else {
+                res.status(404);
+                res.json({error: 'no user with such id'})
+            }
         });
     },
 
-  /* POST - Create new user */
-  createUser: function(req, res, next) {
-    const user = dto2dao(req.body);
-    user.save((err, user) => {
-      if (err) {
-        return res.status(500).json(err.message);
+    /* POST - Create new user */
+    createUser: function(req, res, next) {
+        const user = dto2dao(req.body);
+        user.save((err, user) => {
+            if (err) {
+                return res.status(500).json(err.message);
 
-      }
-      res.status(201).json(dao2dto(user));
-    });
-  },
-
-
-  /* PUT - Update all user */
-  AllUser: function(req, res, next) {
-    res.json({
-      message: "Update a list of users"
-    }, 201);
-  },
+            }
+            res.status(201).json(dao2dto(user));
+        });
+    },
 
 
-  /* PUT - Update one user by id */
-  updateUserByID: function(req, res, next) {
-    var userId = req.params.id;
-    var newFirstname = req.params.firstname;
-    var newLastName = req.params.lastname;
-    var newPosition = req.params.position;
-    var user = User.findById(userId);
-    user.set({
-      firstname: newFirstname,
-      lastname: newLastName,
-      position: newPosition
-    });
-    user.save(function(err, updatedUser) {
-      if (err) return handleError(err);
-      res.send(updatedUser);
-    });
-    res.json({
-      message: "Update user " + userId.text()
-    }, 201);
-  },
+    /* PUT - Update all user */
+    AllUser: function(req, res, next) {
+        res.json({
+            message: 'Update a list of users'
+        }, 201);
+    },
+
+
+    /* PUT - Update one user by id */
+    updateUserByID: function(req, res, next) {
+        var userId = req.params.id;
+        var newFirstname = req.params.firstname;
+        var newLastName = req.params.lastname;
+        var newPosition = req.params.position;
+        var user = User.findById(userId);
+        user.set({
+            firstname: newFirstname,
+            lastname: newLastName,
+            position: newPosition
+        });
+        user.save(function(err, updatedUser) {
+            if (err) return handleError(err);
+            res.send(updatedUser);
+        });
+        res.json({
+            message: 'Update user ' + userId.text()
+        }, 201);
+    },
 
     /* DELETE - Update all user */
     deleteAllUser: function(req, res, next) {
         User.deleteMany();
-        res.json({message:"Delete a list of users"});
+        res.json({message: 'Delete a list of users'});
     },
 
     /* DELETE - Delete one user by id */
@@ -107,10 +117,10 @@ let UserController = {
     },
 
     //Add a user
-    addUser: function (req,res,next) {
+    addUser: function(req, res, next) {
         let user = new User({firstName: 'Polyetch'});
         user.save();
-        res.json({message:"Add polytech"});
+        res.json({message: 'Add polytech'});
     }
 };
 
