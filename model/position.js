0ec2a95f56ac8fb29393c9c2ae636ffbdbe7dcd7
DOLLE <jeremy.dolle34@gmail.com>
2017-11-15 09:39:51 +0100

    add schema position (#3)

---
+++ model/position.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose');
+let positionSchema = mongoose.Schema({
+    position: {
+        lat: {type: String, required: [true, "lat can't be blank"]},
+        lon: {type: String, required: [true, "lon can't be blank"]},
+    }
+});
+let Position = mongoose.model('User',positionSchema);
+module.exports = Position;
